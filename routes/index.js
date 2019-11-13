3d844f24a91f29fe0e64f62a8608c6785bcc6938
jbixon13 <jbixon13@gmail.com>
2019-11-13 01:16:01 -0500

    stable release, structure in place

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res){
+  res.render('index', {
+    title: 'Home'
+  });
+});
+
+router.get('/about', function(req, res){
+  res.render('about', {
+    title: 'About'
+  });
+});
+
+router.get('/contact', function(req, res){
+  res.render('contact', {
+    title: 'Contact'
+  });
+});
+
+router.get('/scrollytest', function(req, res){
+  res.render('post-scrollytest', {
+    title: 'Scrollytest',
+    articleDescription: 'It\'s not doing the sticky thing.',
+    articleDate: 'Nov. 6, 2019'
+  });
+});
+
+router.get('/deckgl', function(req, res){
+  res.render('post-deckgl', {
+    title: 'Deck.gl',
+    articleDate: 'Nov. 5, 2019'
+  });
+});
+
+module.exports = router;
